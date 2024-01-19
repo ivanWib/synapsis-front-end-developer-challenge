e14d4f3b03f1d09a5b24ab2af14d39b76c1dc086
ivanWib <christ.ivan5758@gmail.com>
2024-01-19 18:10:43 +0700

    feat: Fecthing data post

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,70 @@
-import Image from "next/image";
-import PostList from "./PostList";
+"use client";
+import { useState, useEffect } from "react";
+
+type Post = {
+  id?: string;
+  user_id?: string;
+  title?: string;
+  body?: string;
+};
+
+type User = {
+  id?: string;
+  name?: string;
+};
+
+const truncateText = (text: string, maxLength: number) => {
+  if (text.length <= maxLength) {
+    return text;
+  } else {
+    return text.slice(0, maxLength) + "...";
+  }
+};
 
 export default function Home() {
+  const [posts, setPosts] = useState<Post[]>([]);
+  const [users, setUsers] = useState<User[]>([]);
+  const PostUrl = "https://gorest.co.in/public/v2/posts";
+  const UserUrl = "https://gorest.co.in/public/v2/users";
+
+  const fetchPosts = async () => {
+    const res = await fetch(PostUrl);
+    const data = await res.json();
+    setPosts(data);
+  };
+
+  const fetchUsers = async () => {
+    const res = await fetch(UserUrl);
+    const data = await res.json();
+    setUsers(data);
+  };
+
+  useEffect(() => {
+    fetchPosts();
+    fetchUsers();
+  }, []);
+
+  const getUserNameById = (userId: string) => {
+    const user = users.find((u) => u.id === userId);
+    return user ? user.name : "Unknown User";
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p>hello</p>
+    <main>
+      {/* <h1>Blog Posts</h1> */}
+
+      <div className="grid grid-cols-3">
+        {posts &&
+          posts.map((post: Post) => {
+            return (
+              <div key={post.id} className="w-3/4">
+                <h2>{post.title}</h2>
+                <p>{truncateText(post.body || "", 150)}</p>
+                <p>Author: {getUserNameById(post.user_id || "")}</p>
+                <br />
+              </div>
+            );
+          })}
       </div>
     </main>
   );
